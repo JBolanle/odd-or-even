a7a73f0e20346649cc6ce9b7c80ec62ff7d0bea0
Jumoke Bolanle <jumoke.bolanle@gmail.com>
2021-10-24 21:42:58 -0400

    added user input and text
    added user input and dialogue. used try/catch to convert input to integer

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,7 +1,23 @@
 fun main() {
+    println("Odd or Even Calculator")
+    println("Please enter a number: ")
+
+    val userNumber = readLine() ?: "0" //?: sets value to string 0 if null
+    val parsedNumber = try { // try to convert userNumber to Int to prevent crash
+        userNumber.toInt()
+    } catch (e: Exception) {
+        0
+    }
+
+    oddOrEven(parsedNumber)
+    if (oddOrEven(parsedNumber)) {
+        println("The number $parsedNumber is even.")
+    } else {
+        println("The number $parsedNumber is odd.")
+    }
 
 }
 
-fun Int.oddOrEven(number: Int) : Boolean {
-    return (this % 2) == 0
+fun oddOrEven(number: Int) : Boolean {
+    return (number % 2) == 0
 }
